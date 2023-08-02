633572672a14579251586e5247c5b3aa6ac72ad9
AnnaofAmber <126106997+AnnaofAmber@users.noreply.github.com>
2023-08-02 17:11:00 +0000

    Deploying to gh-pages from @ AnnaofAmber/goit-js-hw-09@5df2c327f832d49dd31274c9fd433d9dcfa2b997 🚀

---
+++ 01-color-switcher.6e5769ae.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");function d(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}console.log(d());let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>o.style.backgroundColor=d()),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(()=>{clearInterval(a),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.6e5769ae.js.map
